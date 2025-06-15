e4d5cf903da0cc57db01e1abc3184e7fe1f2c3a8
tomassirio <tomassirio@gmail.com>
2025-06-15 14:21:31 +0200

    Fix Stack's behaviour to throw StackOverflow if size is surpassed

--- src/main/kotlin/system/cpu/utils/SizedStack.kt
+++ src/main/kotlin/system/cpu/utils/SizedStack.kt
@@ -5,8 +5,8 @@ import java.util.Stack
 
 class SizedStack<T>(val maxSize: Int) : Stack<T>() {
     override fun push(item: T): T {
-        while (size >= maxSize) {
-            removeAt(0)
+        if (size >= maxSize) {
+            throw StackOverflowError("Stack has reached maximum size of $maxSize")
         }
         return super.push(item)
     }
