4483b04e6598ce7f2d1b2fd93f360690880b999e
tomassirio <tomassirio@gmail.com>
2025-06-15 14:21:58 +0200

    Delegate LoadRom's responsibilities to SystemController

--- src/main/kotlin/system/cpu/CPU.kt
+++ src/main/kotlin/system/cpu/CPU.kt
@@ -2,13 +2,13 @@ package com.tomassirio.system.cpu
 
 import com.tomassirio.system.cpu.opcode.Command
 import com.tomassirio.system.cpu.opcode.OpCodeTable
-import com.tomassirio.system.register.utils.RegisterSet
 import com.tomassirio.system.memory.Memory
-import com.tomassirio.system.memory.util.toUShortAt
-import com.tomassirio.system.cpu.utils.SizedStack
+import com.tomassirio.system.register.Register
+import com.tomassirio.system.register.utils.RegisterSet
 import com.tomassirio.system.io.DisplayState
 import com.tomassirio.system.io.KeyboardState
-import com.tomassirio.system.register.Register
+import com.tomassirio.system.cpu.utils.SizedStack
+import com.tomassirio.system.memory.util.toUShortAt
 
 class CPU(
     val memory: Memory,
@@ -16,28 +16,14 @@ class CPU(
     val keyboardState: KeyboardState,
     val displayState: DisplayState,
     val pc: Register.ShortRegister,
-    val sp: Register.ByteRegister,
     val I: Register.ShortRegister,
-
     val DT: Register.TimerRegister,
     val ST: Register.TimerRegister,
-
     val stack: SizedStack<UShort>,
 ) {
-    var waitingForKey: Boolean = false
-    var registerToStoreKeyIn: Int? = null
-
-    fun loadRom(data: ByteArray) {
-        data.forEachIndexed { index, byte ->
-            memory.write(
-                memory.startAddress + index,
-                byte.toUByte()
-            )
-        }
-    }
 
     fun runCycle() {
-        if (waitingForKey) return
+        if (keyboardState.isWaitingForKey) return
 
         val opcode = fetch()
         val command = decode(opcode)
@@ -46,7 +32,8 @@ class CPU(
 
     @OptIn(ExperimentalUnsignedTypes::class)
     private fun fetch(): UShort {
-        return memory.read(pc.read().toInt()) { bytes, addr -> bytes.toUShortAt(addr) }
+        return memory.read(pc.read().toInt())
+            { bytes, addr -> bytes.toUShortAt(addr) }
     }
 
     private fun decode(opcode: UShort): Command {
@@ -55,7 +42,7 @@ class CPU(
 
     private fun execute(command: Command, opcode: UShort) {
         command.execute(this, opcode)
-        if(!command.skipsPcIncrement) {
+        if (!command.skipsPcIncrement) {
             pc.write(pc.read().plus(2u).toUShort())
         }
     }
