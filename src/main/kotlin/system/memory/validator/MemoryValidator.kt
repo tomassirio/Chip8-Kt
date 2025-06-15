08243eac8adbf1e10b0bdbd981c5bb32be0e0d2f
tomassirio <tomassirio@gmail.com>
2025-06-15 20:16:13 +0200

    Add MemoryValidator and Memory Accessor

---
+++ src/main/kotlin/system/memory/validator/MemoryValidator.kt
@@ -0,0 +1,20 @@
+package com.tomassirio.system.memory.validator
+
+class MemoryValidator(
+    private val memorySize: Int,
+    private val startAddress: Int,
+    private val isLocked: () -> Boolean
+) {
+    fun validate(address: Int) {
+        require(address in 0 until memorySize) {
+            "Address $address out of bounds (0..${memorySize - 1})"
+        }
+    }
+
+    fun validateWrite(address: Int) {
+        validate(address)
+        if (isLocked() && address < startAddress) {
+            error("Write denied below 0x${startAddress.toString(16)} (boot section locked)")
+        }
+    }
+}
