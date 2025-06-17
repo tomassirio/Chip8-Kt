92b885225b184301e8dda28721b62bfe70f99fa2
tomassirio <tomassirio@gmail.com>
2025-06-17 20:01:16 +0200

    Implement SuperChip8 Commands

--- src/main/kotlin/system/cpu/opcode/OpCodeTable.kt
+++ src/main/kotlin/system/cpu/opcode/OpCodeTable.kt
@@ -9,6 +9,8 @@ import com.tomassirio.system.cpu.opcode.commands.andVxVyCommand
 import com.tomassirio.system.cpu.opcode.commands.callAddrCommand
 import com.tomassirio.system.cpu.opcode.commands.clsCommand
 import com.tomassirio.system.cpu.opcode.commands.drwVxVyNCommand
+import com.tomassirio.system.cpu.opcode.commands.exitCommand
+import com.tomassirio.system.cpu.opcode.commands.highCommand
 import com.tomassirio.system.cpu.opcode.commands.jpAddrCommand
 import com.tomassirio.system.cpu.opcode.commands.jpAddrV0Command
 import com.tomassirio.system.cpu.opcode.commands.ldBVxCommand
@@ -16,15 +18,22 @@ import com.tomassirio.system.cpu.opcode.commands.ldDTVxCommand
 import com.tomassirio.system.cpu.opcode.commands.ldFVxCommand
 import com.tomassirio.system.cpu.opcode.commands.ldIToAddrCommand
 import com.tomassirio.system.cpu.opcode.commands.ldIVxCommand
+import com.tomassirio.system.cpu.opcode.commands.ldRVxCommand
 import com.tomassirio.system.cpu.opcode.commands.ldSTVxCommand
 import com.tomassirio.system.cpu.opcode.commands.ldVxByteCommand
 import com.tomassirio.system.cpu.opcode.commands.ldVxDTCommand
 import com.tomassirio.system.cpu.opcode.commands.ldVxICommand
 import com.tomassirio.system.cpu.opcode.commands.ldVxKCommand
+import com.tomassirio.system.cpu.opcode.commands.ldVxRCommand
 import com.tomassirio.system.cpu.opcode.commands.ldVxVyCommand
+import com.tomassirio.system.cpu.opcode.commands.ldhfVxCommand
+import com.tomassirio.system.cpu.opcode.commands.lowCommand
 import com.tomassirio.system.cpu.opcode.commands.orVxVyCommand
 import com.tomassirio.system.cpu.opcode.commands.retCommand
 import com.tomassirio.system.cpu.opcode.commands.rndVxByteCommand
+import com.tomassirio.system.cpu.opcode.commands.scdNCommand
+import com.tomassirio.system.cpu.opcode.commands.sclCommand
+import com.tomassirio.system.cpu.opcode.commands.scrCommand
 import com.tomassirio.system.cpu.opcode.commands.seVxByteCommand
 import com.tomassirio.system.cpu.opcode.commands.seVxVyCommand
 import com.tomassirio.system.cpu.opcode.commands.shlVxCommand
@@ -96,24 +105,23 @@ object OpCodeTable {
     }
 
     private fun getChip48Command(opcode: UShort): Command {
-        return when(opcode.and(0xFFFu).toUInt()) {
-            0x0000u -> when (opcode.and(0xFFu).toUInt()) {
-//                0xC0u -> SCDCommand //fixme
-//                0x00FBu -> SCRCommand
-//                0x00FCu -> SCLCommand
-//                0x00FDu -> EXITCommand
-//                0x00FEu -> LOWCommand
-//                0x00FFu -> HIGHCommand
+        return when (opcode.toUInt()) {
+            in 0x00C0u..0x00CFu -> scdNCommand()
+            0x00FBu -> scrCommand()
+            0x00FCu -> sclCommand()
+            0x00FDu -> exitCommand()
+            0x00FEu -> lowCommand()
+            0x00FFu -> highCommand()
+            else -> when (opcode.and(0xF000u).toUInt()) {
+                0xF000u -> when (opcode.and(0x00FFu).toUInt()) {
+                    0x30u -> ldhfVxCommand()
+                    0x75u -> ldRVxCommand()
+                    0x85u -> ldVxRCommand()
+                    else -> getCommand(opcode)
+                }
                 else -> getCommand(opcode)
             }
-//            0xD000u -> DRWVxVyNCommand
-            0xF000u -> when (opcode.and(0xFFu).toUInt()) {
-//                0x30u -> LDHFVxCommand
-//                0x75u -> LDRVxCommand
-//                0x85u -> LDRVxCommand
-                else -> getCommand(opcode)
-            }
-            else -> getCommand(opcode)
         }
     }
+
 }
